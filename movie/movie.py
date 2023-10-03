174f63750e7d356a1bad252fab17afe4eb35e9f0
Frédéric Egenscheviller <53983944+frederic-egenscheviller@users.noreply.github.com>
2023-10-03 15:52:46 +0200

    feat: graphql tutorial

--- movie/movie.py
+++ movie/movie.py
@@ -1,6 +1,6 @@
 from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, QueryType, MutationType
 from ariadne.constants import PLAYGROUND_HTML
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 
 import resolvers as r
 
@@ -8,13 +8,25 @@ PORT = 3001
 HOST = '0.0.0.0'
 app = Flask(__name__)
 
-# todo create elements for Ariadne
+type_defs = load_schema_from_path('movie.graphql')
+query = QueryType()
+mutation = MutationType()
+
+movie = ObjectType('Movie')
+actor = ObjectType('Actor')
+
+query.set_field('movie_with_id', r.movie_with_id)
+query.set_field('actor_with_id', r.actor_with_id)
+mutation.set_field('update_movie_rate', r.update_movie_rate)
+movie.set_field('actors', r.resolve_actors_in_movie)
+schema = make_executable_schema(type_defs, movie, query, mutation, actor)
 
 
 # root message
 @app.route("/", methods=['GET'])
 def home():
-    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
+    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>", 200)
+
 
 #####
 # graphql entry points
@@ -22,11 +34,21 @@ def home():
 @app.route('/graphql', methods=['GET'])
 def playground():
     return PLAYGROUND_HTML, 200
-    
+
+
 @app.route('/graphql', methods=['POST'])
 def graphql_server():
-    # todo to complete
+    data = request.get_json()
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=None,
+        debug=app.debug
+    )
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
+
 
 if __name__ == "__main__":
-    print("Server running in port %s"%(PORT))
+    print("Server running in port %s" % (PORT))
     app.run(host=HOST, port=PORT)
