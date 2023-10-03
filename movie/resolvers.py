48ea93caa0361403ef11eb7821a0bf76dc99c56c
Frédéric Egenscheviller <53983944+frederic-egenscheviller@users.noreply.github.com>
2023-10-03 17:48:59 +0200

    feat: movies' methods to graphQL api

--- movie/resolvers.py
+++ movie/resolvers.py
@@ -7,21 +7,76 @@ def movie_with_id(_, info, _id):
         for movie in movies['movies']:
             if movie['id'] == _id:
                 return movie
+        return Exception("Movie ID doesn't exists")
 
 
-def update_movie_rate(_, info, _id, rating):
+def movies_with_rating_better_than_rate(_, info, _rating):
+    movies_list = []
+    print("here")
+    with open('{}/data/movies.json'.format("."), "r") as file:
+        movies = json.load(file)
+        for movie in movies['movies']:
+            if float(movie['rating']) > float(_rating):
+                movies_list.append(movie)
+    return movies_list
+
+
+def movies_with_title(_, info, _title):
+    movies_list = []
+    with open('{}/data/movies.json'.format("."), "r") as file:
+        movies = json.load(file)
+        for movie in movies['movies']:
+            if movie['title'] == _title:
+                movies_list.append(movie)
+    return movies_list
+
+
+def create_movie(_, info, _id, _title, _rating, _director):
+    with open('{}/data/movies.json'.format("."), "r") as rfile:
+        movies = json.load(rfile)
+        for movie in movies['movies']:
+            if movie['id'] == _id:
+                return Exception("Movie ID already exists")
+        newmovie = {'title': _title, 'rating': _rating, 'director': _director, 'id': _id}
+        movies['movies'].append(newmovie)
+        newmovies = movies
+    with open('{}/data/movies.json'.format("."), "w") as wfile:
+        json.dump(newmovies, wfile)
+    return newmovie
+
+
+def delete_movie(_, info, _id):
     newmovies = {}
     newmovie = {}
     with open('{}/data/movies.json'.format("."), "r") as rfile:
         movies = json.load(rfile)
         for movie in movies['movies']:
             if movie['id'] == _id:
-                movie['rating'] = rating
+                newmovie = movie
+                movies['movies'].remove(movie)
+                newmovies = movies
+    if newmovies != movies:
+        with open('{}/data/movies.json'.format("."), "w") as wfile:
+            json.dump(newmovies, wfile)
+        return newmovie
+    return Exception("Movie ID doesn't exists")
+
+
+def update_movie_rate(_, info, _id, _rating):
+    newmovies = {}
+    newmovie = {}
+    with open('{}/data/movies.json'.format("."), "r") as rfile:
+        movies = json.load(rfile)
+        for movie in movies['movies']:
+            if movie['id'] == _id:
+                movie['rating'] = _rating
                 newmovie = movie
                 newmovies = movies
-    with open('{}/data/movies.json'.format("."), "w") as wfile:
-        json.dump(newmovies, wfile)
-    return newmovie
+    if newmovies != movies:
+        with open('{}/data/movies.json'.format("."), "w") as wfile:
+            json.dump(newmovies, wfile)
+        return newmovie
+    return Exception("Movie ID doesn't exists")
 
 
 def actor_with_id(_, info, _id):
